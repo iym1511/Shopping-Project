c422924ff0dddc32fdfc982dfc3996093678d5ce
문일윤 <mun-il-yun@mun-il-yun-ui-MacBookAir.local>
2023-03-08 16:28:34 +0900

    접엇다폇다

--- src/components/Recent.jsx
+++ src/components/Recent.jsx
@@ -6,17 +6,26 @@ const Recent = () => {
 
     const navigate = useNavigate()
 
-    const [fold, setFold] = useState(false)
+    const [fold, setFold] = useState(true)
 
     // 세션에저장한 최근눌렀던 상품 가져옴
     const ssesstionRecent = sessionStorage.getItem("recent")  
     const RecentJSON = JSON.parse(ssesstionRecent)
 
+    const recentFold = () => {
+        if(fold){
+            setFold(false) 
+        }else{
+            setFold(true)
+        }
+        console.log(fold)
+    }
+
     return (  
         <div>
-            <div className="recent-mapbox">
+            <div className={`recent-mapbox ${fold ? 'recent-fold-mapbox' : 'recent-mapbox'}`}>
                 <div className="recent-recentTitle">최근 본 상품</div>
-                <div className='recent-fold'>접기</div>
+                <div className='recent-fold' onClick={recentFold}>접기</div>
                 {  
                     ssesstionRecent != undefined  ? (
                         RecentJSON.map((a,i)=>(
